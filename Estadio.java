287d28ee987fb7e077d42c1e7d12f9c9a7fb13a0
Kauan Gustavo <90695852+K7G9@users.noreply.github.com>
2022-06-03 08:04:31 -0300

    adicionando o projeto Java

---
+++ Estadio.java
@@ -0,0 +1,41 @@
+import java.io.*;
+
+public class Estadio
+{
+    int contarDigitos = 0;
+
+    int Idestadio;
+    int telefone = 10;
+    int cep =  9;
+    String nome;
+    private final int tamNome = 20;
+    private final int tamTelefone = 10;
+    private final int tamCep= 9;
+
+    public Estadio(int idestadio, int telefone, int cep, String nome)
+    {
+        this.Idestadio = idestadio;
+        this.telefone = telefone;
+        this.cep = cep;
+        this.nome = nome;
+    }
+
+    public void ContarDigitos(){
+        if (valor == 10)
+    }
+
+    public int getTamNome()
+    {
+        if()
+        return tamNome;
+    }
+    public int getTamTelefone()
+    {
+        if()
+            return tamNome;
+    }
+    public int getTamCep()
+    {
+            return tamNome;
+    }
+}
